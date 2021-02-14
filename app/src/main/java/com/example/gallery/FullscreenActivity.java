5c95bf24b3ae77658fe3bb144a168945a8bae879
neopetz <61999023+neopetz@users.noreply.github.com>
2021-02-14 23:59:55 +0800

    add ako ng content time and date sa fullScreen

--- app/src/main/java/com/example/gallery/FullscreenActivity.java
+++ app/src/main/java/com/example/gallery/FullscreenActivity.java
@@ -7,6 +7,7 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -24,6 +25,8 @@ public class FullscreenActivity extends AppCompatActivity {
     DatabaseReference reference;
     private String userID;
     private FirebaseUser user;
+    private TextView stamp,content;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +36,14 @@ public class FullscreenActivity extends AppCompatActivity {
         user= FirebaseAuth.getInstance().getCurrentUser();
         userID= user.getUid();
         reference = FirebaseDatabase.getInstance().getReference().child("Users").child(userID);
+        stamp = findViewById(R.id.stamp);
+        content = findViewById(R.id.content);
 
         Bundle bundle = getIntent().getExtras();
         assert bundle != null;
         String imageURL = bundle.getString("IMAGES");
         int position = bundle.getInt("POSITION");
+
 
         reference.child("Posted").addValueEventListener(new ValueEventListener() {
 
@@ -45,12 +51,17 @@ public class FullscreenActivity extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot datasnapshot) {
 
                 String[] imageSlide = new String[(int) datasnapshot.getChildrenCount()];
+                String[] con = new String[(int) datasnapshot.getChildrenCount()];
+                String[] time = new String[(int) datasnapshot.getChildrenCount()];
 
                 int counter = imageSlide.length-1;
 
                 for (DataSnapshot child: datasnapshot.getChildren()) {
 
                     imageSlide[counter] = child.child("Images").child("imageURL").getValue().toString();
+                    con[counter] = child.child("Images").child("Caption").getValue().toString();
+                    time[counter] = child.child("Images").child("Date").getValue().toString()+" "+child.child("Images").child("Time").getValue().toString();
+
                     counter--;
 
                 }
@@ -60,6 +71,28 @@ public class FullscreenActivity extends AppCompatActivity {
                 FullSizeAdapter imageAdapter = new FullSizeAdapter(FullscreenActivity.this,imageSlide);
                 viewPager.setAdapter(imageAdapter);
                 viewPager.setCurrentItem(position);
+                content.setText(con[position]);
+                stamp.setText(time[position]);
+
+                viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+                    @Override
+                    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+                    }
+
+                    @Override
+                    public void onPageSelected(int position) {
+                        //Toast.makeText(FullscreenActivity.this, ""+position, Toast.LENGTH_SHORT).show();
+                        content.setText(con[position]);
+                        stamp.setText(time[position]);
+                    }
+
+                    @Override
+                    public void onPageScrollStateChanged(int state) {
+
+                    }
+                });
+
             }
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
